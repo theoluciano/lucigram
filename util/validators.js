3049d3a0e8e5dd5acb17263f95d9f882d2005977
Theo Luciano <62947477+theoluciano@users.noreply.github.com>
2021-03-01 17:47:54 -0500

    added more validators, error messages

--- util/validators.js
+++ util/validators.js
@@ -16,4 +16,29 @@ module.exports.validateRegisterInput = (
       errors.email = 'Email must be a valid email address.';
     }
   }
-}
+  if(password === ''){
+    errors,password = 'Password must not be empty.'
+  } else if(password !== confirmPassword){
+    errors.confirmPassword = 'Passwords must match.';
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1
+  };
+};
+
+module.exports.validateLoginInput = (username, password) => {
+  const errors = {};
+  if(username.trim() === ''){
+    errors.username = 'Username must not be empty.';
+  }
+  if(password.trim() === ''){
+    errors.password = 'Password must not be empty.';
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1
+  };
+};
