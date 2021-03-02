ac66bf5659b786bca066d1c66ca10b2dd03e6bc1
Theo Luciano <62947477+theoluciano@users.noreply.github.com>
2021-03-02 18:04:28 -0500

    more authentications, some create/delete posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
 module.exports = {
   Query: {
@@ -9,6 +10,36 @@ module.exports = {
       } catch(err){
         throw new Error(err);
       }
+    },
+    async getPost(_, { postId }){
+      try{
+        const post = await Post.findById(postId);
+        if(post){
+          return post;
+        } else {
+          throw new Error('Post not found');
+        }
+      } catch(err){
+        throw new Error(err);
+      }
+    }
+  },
+  Mutation: {
+    async createPost(_, { body }, context){
+      const user = checkAuth(context);
+      console.log(user);
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString()
+
+      });
+
+      const post = await newPost.save();
+
+      return post;
     }
   }
 };
